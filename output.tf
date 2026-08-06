ec2397e2d343750c689ad539a9a46f907059baf6
Tim Butters <timbutters@hotmail.co.uk>
2026-08-06 17:44:48 +0100

    updated resource name output to actual derived name from naming module

--- output.tf
+++ output.tf
@@ -3,14 +3,18 @@ output "resource_outputs" {
     resource_groups = { for key, id in module.vend.resource_group_resource_ids : key => merge(
       local.resource_groups[key],
       {
-        resource_id = id
+        resource_name       = module.naming[key].name
+        resource_group_name = module.naming[key].resource_group_name
+        resource_id         = id
       }
     ) }
 
     virtual_networks = { for key, id in module.vend.virtual_network_resource_ids : key => merge(
       local.virtual_networks[key],
       {
-        resource_id = id
+        resource_name       = module.naming[key].name
+        resource_group_name = module.naming[key].resource_group_name
+        resource_id         = id
       }
     ) }
   }
