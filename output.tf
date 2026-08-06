875aaf9d43ecc3a757c12d6e332b6be4f22f7994
Tim Butters <timbutters@hotmail.co.uk>
2026-08-06 18:03:18 +0100

    updated output of vend module to support upstream modules

--- output.tf
+++ output.tf
@@ -1,20 +1,20 @@
 output "resource_outputs" {
   value = {
-    resource_groups = { for key, id in module.vend.resource_group_resource_ids : key => merge(
-      local.resource_groups[key],
+    resource_groups = { for key, group in local.resource_groups : key => merge(
+      group,
       {
-        resource_name       = module.naming[key].name
-        resource_group_name = module.naming[key].resource_group_name
-        resource_id         = id
+        resource_name       = module.naming["resource_group-${group.location}-${group.resource_name}"].name
+        resource_group_name = module.naming["resource_group-${group.location}-${group.resource_name}"].resource_group_name
+        resource_id         = module.vend.resource_group_resource_ids[key]
       }
     ) }
 
-    virtual_networks = { for key, id in module.vend.virtual_network_resource_ids : key => merge(
-      local.virtual_networks[key],
+    virtual_networks = { for key, vnet in local.virtual_networks : key => merge(
+      vnet,
       {
-        resource_name       = module.naming[key].name
-        resource_group_name = module.naming[key].resource_group_name
-        resource_id         = id
+        resource_name       = module.naming["virtual_network-${vnet.location}-${vnet.resource_name}"].name
+        resource_group_name = module.naming["virtual_network-${vnet.location}-${vnet.resource_name}"].resource_group_name
+        resource_id         = module.vend.virtual_network_resource_ids[key]
       }
     ) }
   }
