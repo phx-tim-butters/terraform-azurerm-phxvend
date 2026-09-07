1ce808f47c84431ba496de052fd44627211796b3
Tim Butters <timbutters@hotmail.co.uk>
2026-09-07 17:18:08 +0100

    existing resource groups for virtual networks

--- locals.virtual_networks.tf
+++ locals.virtual_networks.tf
@@ -6,11 +6,12 @@ locals {
 locals {
   virtual_networks = { for key, vnet in var.virtual_networks : "${vnet.location}-${vnet.resource_name}" => {
 
-    name               = module.naming["virtual_network-${vnet.location}-${vnet.resource_name}"].name
-    key_name           = "${vnet.location}-${vnet.resource_name}"
-    address_space      = vnet.address_space
-    resource_group_key = "${vnet.location}-${vnet.resource_group_short_name}"
-    location           = vnet.location
+    name                         = module.naming["virtual_network-${vnet.location}-${vnet.resource_name}"].name
+    key_name                     = "${vnet.location}-${vnet.resource_name}"
+    address_space                = vnet.address_space
+    resource_group_key           = vnet.existing_resource_group_short_name != null ? null : "${vnet.location}-${vnet.resource_group_short_name}"
+    resource_group_name_existing = vnet.existing_resource_group_short_name != null ? var.existing_resource_groups["${vnet.location}-${vnet.existing_resource_group_short_name}"] : null
+    location                     = vnet.location
 
     dns_servers = vnet.dns_servers
 
