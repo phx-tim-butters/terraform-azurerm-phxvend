5132a201241641474e79859475f3d5a72e6b669a
Tim Butters <timbutters@hotmail.co.uk>
2026-09-08 13:09:56 +0100

    altered to allow for route table association

--- locals.route_tables.tf
+++ locals.route_tables.tf
@@ -1,5 +1,5 @@
 locals {
-  route_tables = length(var.existing_resource_groups) == 0 ? { for key, route_table in var.route_tables : "${route_table.location}-${route_table.resource_name}" => merge(
+  route_tables = { for key, route_table in var.route_tables : "${route_table.location}-${route_table.resource_name}" => merge(
     route_table
     ,
     {
@@ -7,11 +7,12 @@ locals {
       location = try(route_table.location, var.default_location)
 
       key_name                      = "${route_table.location}-${route_table.resource_name}"
-      resource_group_key            = "${route_table.location}-${route_table.resource_group_short_name}"
+      resource_group_key            = route_table.existing_resource_group_short_name != null ? null : "${route_table.location}-${route_table.resource_group_short_name}"
+      resource_group_name_existing  = route_table.existing_resource_group_short_name != null ? var.existing_resource_groups["${route_table.location}-${route_table.existing_resource_group_short_name}"].resource_name : null
       bgp_route_propagation_enabled = strcontains(lower(route_table.resource_name), "gatewaysubnet") || strcontains(lower(route_table.resource_name), "azurefirewall") ? true : route_table.bgp_route_propagation_enabled
       tags                          = merge(var.default_resource_group_tags, try(route_table.tags, {}))
 
       routes = { for route in route_table.routes : route.name => route }
     })
-  } : {}
+  }
 }
